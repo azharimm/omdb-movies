edc30b0d50c6bda6cd1d00485e897096c45187af
azharimm <azhari.marzan@gmail.com>
2021-08-11 11:03:41 +0700

    add dummy data

---
+++ src/data.js
@@ -0,0 +1,109 @@
+export const query = "jadi";
+
+export const items = [
+    {
+        Title: "Naga Bonar Jadi 2",
+        Year: "2007",
+        imdbID: "tt1046961",
+        Type: "movie",
+        Poster: "https://m.media-amazon.com/images/M/MV5BOGMyNzZhYmMtMmMzYi00YmFlLWJkOTMtNTQ4MjMyNDM2NmIzXkEyXkFqcGdeQXVyMjM5OTkzMjI@._V1_SX300.jpg"
+    },
+    {
+        Title: "Jadi Jantem",
+        Year: "1974",
+        imdbID: "tt0367900",
+        Type: "movie",
+        Poster: "https://m.media-amazon.com/images/M/MV5BMWNhYjFiMTQtNTJiMS00ZjE2LTkyOGItMDFkZmM4Y2FiODFlXkEyXkFqcGdeQXVyNTM3MDMyMDQ@._V1_SX300.jpg"
+    },
+    {
+        Title: "Raya Tak Jadi",
+        Year: "2011",
+        imdbID: "tt2074426",
+        Type: "movie",
+        Poster: "https://m.media-amazon.com/images/M/MV5BZmI4MTk0ZWUtZjAyNS00NDU4LTlkZjYtNTg1ODg1ZjdlM2EyXkEyXkFqcGdeQXVyMjgzNDQyMjE@._V1_SX300.jpg"
+    },
+    {
+        Title: "Mau Jadi Apa?",
+        Year: "2017",
+        imdbID: "tt9033464",
+        Type: "movie",
+        Poster: "https://m.media-amazon.com/images/M/MV5BN2FhMTM5ZjctYTc5NS00MTIyLWE4Y2ItNjcxNmNjZThiNDY1XkEyXkFqcGdeQXVyOTI1ODMxNTc@._V1_SX300.jpg"
+    },
+    {
+        Title: "Rotland/Jadi",
+        Year: "2012",
+        imdbID: "tt2691434",
+        Type: "movie",
+        Poster: "N/A"
+    },
+    {
+        Title: "Kabayan Jadi Milyuner",
+        Year: "2010",
+        imdbID: "tt1817710",
+        Type: "movie",
+        Poster: "https://m.media-amazon.com/images/M/MV5BOWJlMmNjYTAtY2JlMS00M2RkLTg1M2EtZTZkZjliYzkzMmE0XkEyXkFqcGdeQXVyMzM2NzMxNTY@._V1_SX300.jpg"
+    },
+    {
+        Title: "Susahnya Jadi Perawan",
+        Year: "2008",
+        imdbID: "tt1190160",
+        Type: "movie",
+        Poster: "https://m.media-amazon.com/images/M/MV5BYTRlYTQ1ODAtZWViZC00OTIwLWI2NjEtZTRhMjc2OWM5MmJiL2ltYWdlXkEyXkFqcGdeQXVyMzM2NzMxNTY@._V1_SX300.jpg"
+    },
+    {
+        Title: "Jelisavetini ljubavni jadi zbog molera",
+        Year: "1972",
+        imdbID: "tt0213742",
+        Type: "movie",
+        Poster: "N/A"
+    },
+    {
+        Title: "Jelisavetini ljubavni jadi",
+        Year: "1985",
+        imdbID: "tt0472384",
+        Type: "movie",
+        Poster: "N/A"
+    },
+    {
+        Title: "Benci jadi cinta",
+        Year: "2006–",
+        imdbID: "tt1325052",
+        Type: "series",
+        Poster: "N/A"
+    }
+];
+
+export const total = 12;
+
+export const item = {
+    Title: "Naga Bonar Jadi 2",
+    Year: "2007",
+    Rated: "N/A",
+    Released: "29 Mar 2007",
+    Runtime: "118 min",
+    Genre: "Comedy",
+    Director: "Deddy Mizwar",
+    Writer: "Musfar Yasin",
+    Actors: "Deddy Mizwar, Tora Sudiro, Wulan Guritno",
+    Plot: "Nagabonar jadi 2 is a comedy movie starring Deddy Mizwar and Tora Sudiro as father and son. This movie is a sequel to 1986 hit movie called Nagabonar. The story about relationship between Nagabonar, a pickpocket who became a self-proclaimed general during Indonesia's war of independence and his only son Bonaga. The movie touches on various subjects concerning the different views of the older generation and the younger, post-modern generation of Indonesia. These subjects include patriotism, traditional values and love.",
+    Language: "Indonesian",
+    Country: "Indonesia",
+    Awards: "6 wins & 5 nominations",
+    Poster: "https://m.media-amazon.com/images/M/MV5BOGMyNzZhYmMtMmMzYi00YmFlLWJkOTMtNTQ4MjMyNDM2NmIzXkEyXkFqcGdeQXVyMjM5OTkzMjI@._V1_SX300.jpg",
+    Ratings: [
+        {
+            Source: "Internet Movie Database",
+            Value: "7.1/10"
+        }
+    ],
+    Metascore: "N/A",
+    imdbRating: "7.1",
+    imdbVotes: "409",
+    imdbID: "tt1046961",
+    Type: "movie",
+    DVD: "N/A",
+    BoxOffice: "N/A",
+    Production: "N/A",
+    Website: "N/A",
+    Response: "True"
+}
